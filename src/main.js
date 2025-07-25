6c8ec1a615b90b064a6fff3ea95eff5bdc352169
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2025-07-25 18:07:06 +0200

    playing with themes and highlighting

--- src/main.js
+++ src/main.js
@@ -15,3 +15,18 @@ document.getElementById("dslInput").addEventListener("keydown", function (e) {
 		textarea.selectionStart = textarea.selectionEnd = start + 1;
 	}
 });
+/*
+const themeSelector = document.getElementById("themeSwitcher");
+const wrapper = document.getElementById("output");
+
+themeSelector.addEventListener("change", function () {
+	wrapper.className = this.value;
+});
+*/
+
+document.querySelectorAll('.button-group-2 button').forEach(button => {
+	button.addEventListener('click', () => {
+		const theme = button.getAttribute('data-theme'); // get theme from button attr
+		document.body.className = theme; // replace entire class list with theme class
+	});
+});
